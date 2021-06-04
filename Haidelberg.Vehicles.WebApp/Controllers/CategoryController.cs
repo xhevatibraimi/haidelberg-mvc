92133d19e66656b1cc9424794540cc9cfd6a568f
xhevat Ibraimi <xhevat.ibraimi@gmail.com>
2021-06-04 21:05:26 +0200

    added CRUD operations

---
+++ Haidelberg.Vehicles.WebApp/Controllers/CategoryController.cs
@@ -0,0 +1,61 @@
+﻿using Haidelberg.Vehicles.DataAccess.EF;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Haidelberg.Vehicles.WebApp.Controllers
+{
+    public class CategoryController : Controller
+    {
+        private readonly DatabaseContext _context;
+
+        public CategoryController(DatabaseContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult Index()
+        {
+            var categories = _context.Categories.ToList();
+            //ViewBag.Categories = "a,b,c,d,e";
+            //ViewData["Categories"] = "a,b,c,d,e";
+            //TempData["Text"] = "hello from index";
+            //return RedirectToAction("Redirect");
+            return View(categories);
+        }
+
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(Category model)
+        {
+            _context.Categories.Add(model);
+            _context.SaveChanges();
+            return RedirectToAction("Details", new { Id = model.Id });
+        }
+
+        // /Category/Details/123
+        public IActionResult Details(int id)
+        {
+            var model = _context.Categories.FirstOrDefault(x => x.Id == id);
+            if (model == null)
+            {
+                return RedirectToAction("Index");
+            }
+
+            return View(model);
+        }
+
+        //public IActionResult Redirect()
+        //{
+        //    ViewBag.Text = TempData["Text"];
+        //    return View();
+        //}
+    }
+}
