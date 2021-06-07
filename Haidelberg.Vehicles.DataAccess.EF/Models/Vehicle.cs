b66e8db96bcba75e4c96f521616d01af0971ab05
xhevat Ibraimi <xhevat.ibraimi@gmail.com>
2021-06-07 21:01:19 +0200

    added vehicle create view

--- Haidelberg.Vehicles.DataAccess.EF/Models/Vehicle.cs
+++ Haidelberg.Vehicles.DataAccess.EF/Models/Vehicle.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 
 namespace Haidelberg.Vehicles.DataAccess.EF
@@ -6,12 +7,16 @@ namespace Haidelberg.Vehicles.DataAccess.EF
     public class Vehicle
     {
         public int Id { get; set; }
-        //[Required(ErrorMessage = "Production Year is a required field!")]
-        //[Display(Name = "Production Year")]
+        [Required(ErrorMessage = "Production Year is a required field!")]
+        [Range(2000, 2100, ErrorMessage = "The value should be between 2000 and 2100")]
         public int ProductionYear { get; set; }
+        [Required]
         public DateTime LastRegistrationDate { get; set; }
+        [Required]
         public string Model { get; set; }
+        [Required]
         public string LicencePlate { get; set; }
+        [Required]
         public int CategoryId { get; set; }
 
         [ForeignKey("CategoryId")]
--- Haidelberg.Vehicles.WebApp/Controllers/VehiclesController.cs
+++ Haidelberg.Vehicles.WebApp/Controllers/VehiclesController.cs
@@ -1,6 +1,8 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Haidelberg.Vehicles.DataAccess.EF;
 using System.Linq;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
 
 namespace Haidelberg.Vehicles.WebApp.Controllers
 {
@@ -13,10 +15,34 @@ namespace Haidelberg.Vehicles.WebApp.Controllers
             _context = context;
         }
 
+        [HttpGet]
+        public IActionResult Index()
+        {
+            var vehicles = _context.Vehicles.Include(x => x.Category).ToList();
+            return View(vehicles);
+        }
+
+        [HttpGet]
         public IActionResult Create()
         {
             ViewBag.Categories = _context.Categories.ToList();
-            return View();
-        } 
+            return View(new Vehicle());
+        }
+
+        [HttpPost]
+        public IActionResult Create(Vehicle vehicle)
+        {
+            if (!(ModelState.IsValid && _context.Categories.Any(x => x.Id == vehicle.CategoryId)))
+            {
+                ViewBag.ErrorMessage = ModelState.FirstOrDefault(x => x.Value.Errors.Any()).Value.Errors.FirstOrDefault().ErrorMessage;
+                ViewBag.Categories = _context.Categories.ToList();
+                return View(vehicle);
+            }
+
+            _context.Add(vehicle);
+            _context.SaveChanges();
+
+            return RedirectToAction("Details");
+        }
     }
 }
