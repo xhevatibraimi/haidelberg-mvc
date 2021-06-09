466de3654786d3a40fa7be18794f97a0d77c5ee6
xhevat Ibraimi <xhevat.ibraimi@gmail.com>
2021-06-09 19:33:54 +0200

    added delete view

--- Haidelberg.Vehicles.WebApp/Controllers/VehiclesController.cs
+++ Haidelberg.Vehicles.WebApp/Controllers/VehiclesController.cs
@@ -26,7 +26,7 @@ namespace Haidelberg.Vehicles.WebApp.Controllers
         public IActionResult Create()
         {
             ViewBag.Categories = _context.Categories.ToList();
-            return View(new Vehicle());
+            return View(new Vehicle { LastRegistrationDate = System.DateTime.UtcNow });
         }
 
         [HttpPost]
@@ -43,6 +43,86 @@ namespace Haidelberg.Vehicles.WebApp.Controllers
             _context.SaveChanges();
 
             return RedirectToAction("Details");
+        }
+
+        [HttpGet]
+        public IActionResult Details(int id)
+        {
+            var dbVehicle = _context.Vehicles.Include(x => x.Category).FirstOrDefault(x => x.Id == id);
+            if (dbVehicle == null)
+            {
+                return RedirectToAction("Index");
+            }
+
+            return View(dbVehicle);
+        }
+
+        [HttpGet]
+        public IActionResult Edit(int id)
+        {
+            var dbVehicle = _context.Vehicles.FirstOrDefault(x => x.Id == id);
+            if (dbVehicle == null)
+            {
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.Categories = _context.Categories.ToList();
+            return View(dbVehicle);
+        }
+
+        [HttpPost]
+        public IActionResult Edit(int id, Vehicle vehicle)
+        {
+            var dbVehicle = _context.Vehicles.FirstOrDefault(x => x.Id == id);
+
+            if (dbVehicle == null)
+            {
+                return RedirectToAction("Index");
+            }
+
+            if (!(ModelState.IsValid && _context.Categories.Any(x => x.Id == vehicle.CategoryId)))
+            {
+                ViewBag.ErrorMessages = ModelState.SelectMany(x => x.Value.Errors).Select(x => x.ErrorMessage).ToList();
+                ViewBag.Categories = _context.Categories.ToList();
+                return View(vehicle);
+            }
+
+            dbVehicle.CategoryId = vehicle.CategoryId;
+            dbVehicle.LastRegistrationDate = vehicle.LastRegistrationDate;
+            dbVehicle.LicencePlate = vehicle.LicencePlate;
+            dbVehicle.Model = vehicle.Model;
+            dbVehicle.ProductionYear = vehicle.ProductionYear;
+
+            _context.SaveChanges();
+
+            return RedirectToAction("Details", new { Id = vehicle.Id });
+        }
+
+        [HttpGet]
+        public IActionResult Delete(int id)
+        {
+            var dbVehicle = _context.Vehicles.Include(x => x.Category).FirstOrDefault(x => x.Id == id);
+            if (dbVehicle == null)
+            {
+                return RedirectToAction("Index");
+            }
+
+            return View(dbVehicle);
+        }
+
+        [HttpPost]
+        public IActionResult Delete(int id, Vehicle vehicle)
+        {
+            var dbVehicle = _context.Vehicles.FirstOrDefault(x => x.Id == id);
+            if (dbVehicle == null)
+            {
+                return RedirectToAction("Index");
+            }
+
+            _context.Remove(dbVehicle);
+            _context.SaveChanges();
+
+            return RedirectToAction("Index");
         }
     }
 }
