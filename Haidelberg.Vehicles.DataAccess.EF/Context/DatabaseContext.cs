f1416c9f51ffbadf9d437cc804bd70dbd3270b81
xhevat Ibraimi <xhevat.ibraimi@gmail.com>
2021-06-23 17:56:08 +0200

    added EF changes for aspnet identity

--- Haidelberg.Vehicles.DataAccess.EF/Context/DatabaseContext.cs
+++ Haidelberg.Vehicles.DataAccess.EF/Context/DatabaseContext.cs
@@ -1,8 +1,9 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
 
 namespace Haidelberg.Vehicles.DataAccess.EF
 {
-    public class DatabaseContext : DbContext
+    public class DatabaseContext : IdentityDbContext<User>
     {
         public DatabaseContext(DbContextOptions<DatabaseContext> options)
             : base(options)
---
+++ Haidelberg.Vehicles.DataAccess.EF/Models/User.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Haidelberg.Vehicles.DataAccess.EF
+{
+    public class User : IdentityUser
+    {
+        public int? EmployeeId { get; set; }
+
+        [ForeignKey("EmployeeId")]
+        public virtual Employee Employee { get; set; }
+    }
+}
